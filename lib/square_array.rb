ef3dfc1e00388c14747357b1c9de672888443985
sasha walker <sashawalker31@gmail.com>
2019-07-31 15:44:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
-  array=[1,2,3]
-  p square_array(array)
-  new_numbers=[]
+  counter = 0
+   new_array = []
+  while counter < array.length
+   new_array << array[counter] ** 2
+    counter += 1
+  end
+  new_array
 end
